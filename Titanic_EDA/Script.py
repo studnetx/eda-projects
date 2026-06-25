da0d627a621996e3cdaa53689b093406b30ca0b6
Majid Mehmood <majidstudentx@gmail.com>
2026-06-25 22:27:19 +0500

    Implement variable classification and visualization
    Added a function to classify variables into categorical and numerical types, and visualized the results with various plots.

--- Titanic_EDA/Script.py
+++ Titanic_EDA/Script.py
@@ -30,3 +30,53 @@ print(df.duplicated().sum())
 print("Info:")
 print(df.info())
 
+
+def classify_variables(data):
+    """Variables ko categorical aur numerical mein divide karna"""
+    cat = []
+    num = []
+    for col in data.columns:
+        if data[col].dtype == 'object':
+            cat.append(col)
+        elif data[col].dtype in ['int64', 'float64']:
+            if data[col].nunique() < 10:  # Agar unique values kam hain toh treat as categorical
+                cat.append(col)
+            else:
+                num.append(col)
+    return cat, num
+
+
+cat_vars, num_vars = classify_variables(df)
+print("="*79)
+print("🎨 Variable Classification:")
+print("="*79)
+print("Categorical_Features :\n",cat_vars)
+print("Numerical_Features :\n",num_vars)
+
+
+# dashboardign by graphs
+fig , ax =plt.subplots(2,2 ,figsize=(10,10))
+# missing Values
+sns.heatmap(df.isnull(),ax=ax[0,0])
+
+ax[0,0].set_title("Missing Values Map (Yellow = Missing)")
+ax[0,1].bar(['catergorical','numerical'],[len(cat_vars),len(num_vars)])
+ax[0,1].set_title('Variable Types Count')
+ax[0,1].set_ylabel('Count')
+
+
+
+# Data types pie chart
+ax[1,0].pie(df.dtypes.value_counts().values,labels=df.dtypes.value_counts().index,autopct='%1.1f%%')
+ax[1,0].set_title('Data Types Distribution')
+
+unique_counts = df.nunique().sort_values(ascending=False)
+
+ax[1,1].barh(range(len(unique_counts)),unique_counts.values)
+ax[1,1].set_yticks(range(len(unique_counts)))
+ax[1,1].set_yticklabels(unique_counts.index, fontsize=8)
+ax[1,1].set_title('Unique Values per Column')
+ax[1,1].set_xlabel('Unique Values Count')
+plt.tight_layout()
+plt.show()
+
